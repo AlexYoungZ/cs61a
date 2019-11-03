4af887634626d76580e40b22db56eb262a1894b6
AlexYoungZ <zhangsiy2015@163.com>
2019-11-03 15:30:17 +0800

    completed while binary unsure

--- Labs/lab01/lab01_extra.py
+++ Labs/lab01/lab01_extra.py
@@ -15,6 +15,17 @@ def falling(n, k):
     4
     """
     "*** YOUR CODE HERE ***"
+    fac=n
+    if k==0:
+        return 1
+    if k==1:
+        return n
+    else:
+        while k>1:
+            fac*=n-1
+            n-=1
+            k-=1
+    return fac
 
 def double_eights(n):
     """Return true if n has two eights in a row.
@@ -32,3 +43,102 @@ def double_eights(n):
     False
     """
     "*** YOUR CODE HERE ***"
+    cur=0
+    cur_eight=False
+    while(n>0):
+        cur=n%10
+        if (cur==8):
+            if cur_eight==True:
+                return True
+            else:
+                cur_eight=True
+        else:
+            cur_eight=False
+        n=n//10
+    return False
+
+
+# Guessing Game
+
+from random import randint
+
+LOWER = 1
+UPPER = 10
+
+def guess_random():
+    """Guess randomly and return the number of guesses."""
+    prompt_for_number(LOWER, UPPER)   # asks the user to choose a number
+    num_guesses, correct = 0, False
+    while not correct:
+        guess = randint(LOWER, UPPER) # randomly guess
+        correct = is_correct(guess)   # ask user if guess is correct
+        num_guesses = num_guesses + 1
+    return num_guesses
+
+def guess_linear():
+    """Guess in increasing order and return the number of guesses."""
+    prompt_for_number(LOWER, UPPER)
+    num_guesses = 0
+    guess = LOWER
+    "*** YOUR CODE HERE ***"
+    correct=False
+    while not correct:
+        num_guesses+=1
+        correct=is_correct(guess)
+        guess=guess+1
+    return num_guesses
+
+def guess_binary():
+    """Return the number of attempted guesses. Implement a faster search
+    algorithm that asks the user whether a guess is less than or greater than
+    the correct number.
+
+    Hint: If you know the guess is greater than the correct number, then your
+    algorithm doesn't need to try numbers that are greater than guess.
+    """
+    prompt_for_number(LOWER, UPPER)
+    num_guesses = 0
+    lower, upper = LOWER, UPPER
+    guess = (lower + upper) // 2
+    "*** YOUR CODE HERE ***"
+    correct=False
+    while not correct:
+        if is_too_high(guess):
+            guess=(lower+guess)//2
+            num_guesses+=1
+            correct=is_correct(guess)
+        else:
+            guess=(guess+upper)//2
+            num_guesses+=1
+            correct=is_correct(guess)
+    return num_guesses
+
+# Receive user input. You do not need to understand the code below this line.
+
+def prompt_for_number(lower, upper):
+    """Prompt the user for a number between lower and upper. Return None."""
+    is_valid_number = False
+    while not is_valid_number:
+        # You don't need to understand the following two lines.
+        number = input('Pick an integer between {0} and {1} (inclusive) for me to guess: '.format(lower, upper))
+        number = int(number)
+        if lower <= number <= upper:
+            is_valid_number = True
+
+def is_correct(guess):
+    """Ask the user if a guess is correct and return whether they respond y."""
+    return is_yes('Is {0} your number? [y/n] '.format(guess))
+
+def is_too_high(guess):
+    """Ask the user if a guess is too high and return whether they say yes."""
+    return is_yes('Is {0} too high? [y/n] '.format(guess))
+
+def is_yes(prompt):
+    """Ask the user a yes or no question and return whether they say yes."""
+    while True: # This while statement will loop until a "return" is reached.
+        yes_no = input(prompt).strip()
+        if yes_no == 'y':
+            return True
+        elif yes_no == 'n':
+            return False
+        print('Please type y or n and press return/enter')
