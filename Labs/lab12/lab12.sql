8c9b4b2ea22bc60e4ade9a51ddcded3fb514e302
AlexYoungZ <zhangsiy2015@163.com>
2019-11-17 15:07:17 +0800

    optional questions later

---
+++ Labs/lab12/lab12.sql
@@ -0,0 +1,13 @@
+.read fa18data.sql
+
+-- Q2
+CREATE TABLE obedience AS
+    select seven, denero from students;
+-- Q3
+CREATE TABLE smallest_int AS
+    select time ,smallest from students where smallest>13 order by smallest limit 20;
+-- Q4
+CREATE TABLE matchmaker AS
+    select A.pet, A.song, A.color, B.color from
+    students as A,students as B where
+    A.pet=B.pet and A.song=B.song and A.time<B.time;
